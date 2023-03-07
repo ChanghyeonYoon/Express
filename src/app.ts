ec529c6aed3d11454ec4e266608d13c43f09be59
ChanghyeonYoon <hyeon120411@gmail.com>
2023-03-07 18:05:32 +0000

    Feat : Middleware

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,17 @@ const path = require("path");
 
 const app = express();
 
+// Middleware
+app.use((req: any, res: any, next: any) => {
+    console.log('모든 요청에 실행 됨');
+    next();
+})
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req: any, res: any) => {
     res.sendFile(path.join(__dirname, './index.html'))
 })
-
 
 
 app.listen(app.get('port'), () => {
