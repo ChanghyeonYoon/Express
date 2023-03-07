ae19204baaec49bff0c96a11ca96051b606ffc0d
ChanghyeonYoon <hyeon120411@gmail.com>
2023-03-07 17:56:29 +0000

    Feat : Send HTML

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req: any, res: any) => {
-    res.send('hello express')
+    res.sendFile(path.join(__dirname, './index.html'))
 })
+
+
 
 app.listen(app.get('port'), () => {
     console.log(`server is running on port ${app.get('port')} ...`)
